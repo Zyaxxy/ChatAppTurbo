392f23250fbead48e8a3b307fd02130a66e5703a
Zyaxxy <utkarshjaiswal285@gmail.com>
2025-11-13 22:49:36 +0530

    Added Shadcn Components

---
+++ apps/web/app/chat/[roomId]/page.tsx
@@ -0,0 +1,9 @@
+export default function ChatPage() {
+  return (
+    <div className="flex items-center justify-center min-h-svh">
+      <div className="flex flex-col items-center justify-center gap-4">
+        Chat Room
+      </div>
+    </div>
+  )
+}   
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,18 @@
+"use client"
 import { Button } from "@workspace/ui/components/button"
+import { Input } from "@workspace/ui/components/input"
+import { useRouter } from "next/navigation"
 
 export default function Page() {
+  const router = useRouter();
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <Input placeholder="Room ID" className="w-50px h-25px" />
+        <Button size="lg" variant={"default"} onClick={()=>{
+          router.push("/chat/[roomId]")
+        }}>Join Room</Button>
       </div>
     </div>
-  )
+  ) 
 }
