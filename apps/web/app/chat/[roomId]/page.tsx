9f10d467a2443ab5dc67f07bce64e2cd28118237
Zyaxxy <utkarshjaiswal285@gmail.com>
2025-11-26 23:51:01 +0530

    Added authentication

--- apps/web/app/chat/[roomId]/page.tsx
+++ apps/web/app/chat/[roomId]/page.tsx
@@ -1,9 +1,19 @@
-export default function ChatPage() {
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../../../lib/auth";
+
+export default async function ChatPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         Chat Room
       </div>
     </div>
-  )
-}   
+  );
+}
